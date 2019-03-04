73b1ca3bb91ad40866d33031eac47d89f5f0da7a
junhua <junhua.lin@jfz.com>
2019-03-04 16:06:02 +0800

    feat(SPManager):添加SharedPreferences工具类，通用数据存储

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 ### 网络相关
 #### [SSLUtils](https://github.com/JunhuaLin/AndroidUtilsLibrary/blob/master/app/src/main/java/cn/junhua/android/utilslibrary/ssl/SSLUtils.java) 证书工具类，用于处理OKHttp证书设置的工具类
 #### [SimpleCacheInterceptor](https://github.com/JunhuaLin/SimpleCacheInterceptor) OKHttp3缓存拦截器用于缓存http请求结果。
+
+### 文件相关
+#### [SPManager](https://github.com/JunhuaLin/AndroidUtilsLibrary/blob/master/app/src/main/java/cn/junhua/android/utilslibrary/io/SPManager.java) SharedPreferences工具类，通用数据存储
---
+++ app/src/main/java/cn/junhua/android/utilslibrary/io/SPManager.java
@@ -0,0 +1,161 @@
+package cn.junhua.android.utilslibrary.io;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.ref.SoftReference;
+import java.util.Map;
+
+/**
+ * SharedPreferences工具类，通用数据存储
+ *
+ * @author junhua.lin@jinfuzi.com<br/>
+ * CREATED 2019/3/2 15:18
+ */
+public class SPManager {
+    @SuppressLint("StaticFieldLeak")
+    private static Context CONTEXT;
+    private String name;
+    private SoftReference<SharedPreferences> softSP = new SoftReference<>(null);
+
+    private SPManager() {
+        this.name = "sp_manager";
+    }
+
+    private SPManager(String name) {
+        this.name = name;
+    }
+
+    public static SPManager getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    /**
+     * 只需要初始化一次即可
+     *
+     * @param context Context
+     */
+    public static void init(Context context) {
+        CONTEXT = context.getApplicationContext();
+    }
+
+    /**
+     * 此处用来处理非通用情况下的存储情况
+     *
+     * @param name 文件名
+     * @return SPManager
+     */
+    public static SPManager newInstance(String name) {
+        return new SPManager(name);
+    }
+
+    private SharedPreferences getSP() {
+        if (CONTEXT == null) {
+            throw new IllegalStateException("初始化SPManager啊，大兄弟！");
+        }
+
+        SharedPreferences sp = softSP.get();
+        if (sp == null) {
+            sp = CONTEXT.getSharedPreferences(String.format(CONTEXT.getPackageName() + ".%s.sp", name), Context.MODE_MULTI_PROCESS);
+            softSP = new SoftReference<>(sp);
+        }
+        return sp;
+    }
+
+    public void put(@NonNull String key, @Nullable String vale) {
+        SharedPreferences.Editor editor = getSP().edit();
+        editor.putString(key, vale);
+        editor.apply();
+    }
+
+    public void put(@NonNull String key, boolean vale) {
+        put(key, String.valueOf(vale));
+    }
+
+    public void put(@NonNull String key, float vale) {
+        put(key, String.valueOf(vale));
+    }
+
+    public void put(@NonNull String key, int vale) {
+        put(key, String.valueOf(vale));
+    }
+
+    public void put(@NonNull String key, long vale) {
+        put(key, String.valueOf(vale));
+    }
+
+    public <T> T get(@NonNull String key, T defaultValue) {
+        String className = defaultValue.getClass().getName();
+        SharedPreferences sp = getSP();
+        String value = sp.getString(key, null);
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            Object temp;
+            if (className.equals(String.class.getName())) {
+                return (T) value;
+            }
+
+            if (className.equals(Integer.class.getName())
+                    || className.equals(int.class.getName())) {
+                temp = Integer.parseInt(value);
+                return (T) temp;
+            }
+
+            if (className.equals(long.class.getName())
+                    || className.equals(Long.class.getName())) {
+                temp = Long.parseLong(value);
+                return (T) temp;
+            }
+
+            if (className.equals(float.class.getName())
+                    || className.equals(Float.class.getName())) {
+                temp = Float.parseFloat(value);
+                return (T) temp;
+            }
+
+            if (className.equals(boolean.class.getName())
+                    || className.equals(Boolean.class.getName())) {
+                if (value.equalsIgnoreCase("false")
+                        || value.equalsIgnoreCase("true")) {
+                    temp = Boolean.parseBoolean(value);
+                    return (T) temp;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            String msg = String.format("SPManger.get(%s,%s) = \"%s\" cannot be cast to %s", key, defaultValue, value, className);
+            throw new IllegalArgumentException(msg);
+        }
+
+        return defaultValue;
+    }
+
+    public boolean contains(@NonNull String key) {
+        return getSP().contains(key);
+    }
+
+    public void remove(@NonNull String key) {
+        getSP().edit().remove(key).apply();
+    }
+
+    public void removeAll() {
+        getSP().edit().clear().apply();
+    }
+
+    public Map<String, ?> getAll() {
+        return getSP().getAll();
+    }
+
+    private static final class InstanceHolder {
+        @SuppressLint("StaticFieldLeak")
+        private final static SPManager INSTANCE = new SPManager();
+    }
+}
